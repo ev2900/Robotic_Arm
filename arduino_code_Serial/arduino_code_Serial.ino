b65041fb1dc2b2baabb3d538701908cb7fc90dc7
ev2900 <ev2900@gmail.com>
2014-09-25 20:13:46 -0400

    added arduino code and code with serial

---
+++ arduino_code_Serial/arduino_code_Serial.ino
@@ -0,0 +1,33 @@
+// Shoulder Servo on digital pin 9
+// Shoulder Elbow on digital pin 10
+
+#include <Servo.h>
+
+Servo shoulder;
+Servo elbow;
+
+void setup() {
+  
+  shoulder.attach(9);
+  elbow.attach(10);
+  Serial.begin(9600);
+   
+}
+
+void loop() {
+ 
+  if(Serial.available()) {
+    
+    int servoAngle = Serial.read();
+    
+    servoAngles[nextServo] = servoAngle;
+    nextServo++;
+    
+    if(nextServo > 1) {
+      nextServo = 0;
+    }
+    
+    shoulder.write(ServoAngles[0]);
+    elbow.write[1]);
+  }
+}
